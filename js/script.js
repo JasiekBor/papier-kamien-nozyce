d5c6b8dc153944c1b256cdc08bf6811a64b40b8e
Jan Borowski <contact@jbdeveloper.eu>
2022-04-08 17:52:48 +0200

    Add first text

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+let computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
